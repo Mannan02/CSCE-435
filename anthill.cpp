144486e5e8e23c975f058d3da59bff00167f9fb9
Mannan Mendiratta <mannanmendiratta99@gmail.com>
2021-03-23 19:19:36 -0500

    Done

--- anthill.cpp
+++ anthill.cpp
@@ -309,7 +309,6 @@ int main (int argc, char **argv) {
         loc2 = maxinfo[0].loc2;
         max_val = maxinfo[0].val;
         for (int i = 1; i < 16; ++i){
-            printf("%d %f\n", i, maxinfo[i].val);
             if (maxinfo[i].val > max_val){
                 max_val = maxinfo[i].val;
                 loc1 = maxinfo[i].loc1;
